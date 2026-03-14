80dfff28242b0a7e598caeddaa83b5c4290ce659
itzpiro <ayushqw4469@gmail.com>
2026-03-14 19:22:14 +0530

    change pass

--- src/components/SecretCodeCard.jsx
+++ src/components/SecretCodeCard.jsx
@@ -19,7 +19,7 @@ export default function SecretCodeCard({ onCorrect }) {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        if (code === '1234') {
+        if (code === '486') {
             onCorrect()
         } else {
             setError("Oops! That’s not the right code.")
